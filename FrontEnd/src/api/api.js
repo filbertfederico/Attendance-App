ed409db284f898fb77d44defdaab8cbb1df1fe22
filbertfederico <filbert.federico2@gmail.com>
2025-11-27 14:33:19 +0700

    deploy multi approval 1.2 re-attach BE

--- FrontEnd/src/api/api.js
+++ FrontEnd/src/api/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 export const api = axios.create({
   // DEPLOY
-  // baseURL: process.env.REACT_APP_API_URL || "https://attendance-app-vwy8.onrender.com"
+  baseURL: process.env.REACT_APP_API_URL || "https://attendance-app-vwy8.onrender.com"
   // LOCAL
-    baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:10000/"
+    // baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:10000/"
 });
 
 // Firebase token
