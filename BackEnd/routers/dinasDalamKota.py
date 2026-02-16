c01f65e64520c475697ec85ab2ffc0fb84d2cd93
filbertfederico <filbert.federico2@gmail.com>
2026-02-16 21:35:10 +0700

    divhead change 2.1.2  (fix wrong import path)

--- BackEnd/routers/dinasDalamKota.py
+++ BackEnd/routers/dinasDalamKota.py
@@ -7,10 +7,9 @@ from datetime import datetime
 
 from BackEnd.models import DinasDalamKota
 from BackEnd.database import get_db
-from .auth import get_current_user
+from .auth import get_current_user, require_admin
 
 from .utils import is_div_head_of_division, is_hrd_head, is_hrd_staff
-from auth import require_admin
 
 router = APIRouter()
 
